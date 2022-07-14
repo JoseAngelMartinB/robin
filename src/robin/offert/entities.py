0725fbfe8f4839d296b64d2e9b6224925cc7e1ff
luis <luis.jimenez@uclm.es>
2022-07-14 18:53:22 +0200

    add classes Station, TimeSlot and Corridor

---
+++ src/robin/offert/entities.py
@@ -0,0 +1,27 @@
+class Station (object):
+    def __init__(self,id:int,name:str,shortname:str):
+        self.id=id
+        self.name=name
+        self.shortname=shortname
+    
+    def __str__(self):
+        return f'[{self.id},{self.name},{self.shortname}]'
+    
+    
+class TimeSlot (object):
+    def __init__(self,id:int,classmark:float,size:float):
+        self.id=id
+        self.classmark=classmark
+        self.size=size
+        
+    def __str__(self):
+        return f'[{self.id},{self.classmark},{self.size}]'
+    
+class Corridor (objet):
+    def __init__(self,id:int,liststation:list=[]):
+        self.id=id
+        self.liststation=liststation
+    def insertStation(self,station:Station):
+        self.liststation.append(station)
+    def __str__(self):
+        return f'[{self.id},{self.liststation}]'
