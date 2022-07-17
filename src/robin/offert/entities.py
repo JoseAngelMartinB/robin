8b98a466b33e99c6d6a9218ce7e190b6e5f3ba07
Luis Jiménez <luis.jimenez@uclm.es>
2022-07-17 20:20:05 +0200

    Add scraping renfe web and Line

--- src/robin/offert/entities.py
+++ src/robin/offert/entities.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Tuple
 class Station (object):
     def __init__(self,id:int,name:str,shortname:str):
         self.id=id
@@ -26,3 +26,22 @@ class Corridor (object):
         self.liststation.append(station)
     def __str__(self):
         return f'[{self.id},{self.liststation}]'
+    
+class Line (object):
+    def __init__(self,id:int,corr:Corridor,lstation:List[Station]=[],ltimetable:List[Tuple]=[]):
+        self.id=id
+        self.corr=corr
+        self.lstation=lstation
+        self.timetable=ltimetable # list of tuple (AT,DT)
+        
+    def insertStation(self,station:Station,AT:float,DT:float):
+        self.lstation.append(station)
+        self.timetable.append((AT,DT))
+        
+    
+    
+if __name__=='__main__':
+    station1=Station(1,'station1','s1')
+    station2=Station(2,'station2','s2')
+    c=Corridor(1,[station1,station2])
+    print(c)
