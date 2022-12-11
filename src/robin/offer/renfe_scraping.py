f2b0798fd2b2257049466e69c6e344a113cf9a5d
DavidMunozValero <david.munoz6@alu.uclm.es>
2022-12-11 16:16:20 +0100

    Scraping multiple days and saving into pandas df

--- src/robin/offer/renfe_scraping.py
+++ src/robin/offer/renfe_scraping.py
@@ -2,6 +2,26 @@ from scraping.renfe import *
 import pandas as pd
 import datetime
 
+
+def format_time(x):
+    """ Function receives "x", a string with time formatted as "2 h. 30 m." and returns a timedelta object """
+    h, m = filter(lambda t: is_number(t), x.split(" "))
+    return datetime.timedelta(hours=int(h), minutes=int(m))
+
+
+def to_dataframe(s, d):
+    table = get_table(s)
+
+    dfs = pd.DataFrame(table, columns=['Train', 'Stops', 'Departure', 'Duration', 'Price'])
+    dfs = dfs[dfs["Train"].apply(lambda x: "AVE" in x)].reset_index(drop=True)
+
+    dfs['Duration'] = dfs['Duration'].apply(lambda x: format_time(x))
+    dfs['Departure'] = dfs['Departure'].apply(lambda x: datetime.datetime.strptime(str(d) + "-" + x, '%Y-%m-%d-%H.%M'))
+    dfs['Arrival'] = dfs['Departure'] + dfs['Duration']
+
+    return dfs
+
+
 # Renfe search menu
 url = "https://www.renfe.com/content/renfe/es/es/viajar/informacion-util/horarios/app-horarios.html"
 
@@ -32,22 +52,38 @@ req = requests.get(url)
 soup = BeautifulSoup(req.text, 'html.parser')
 
 # Retrieve date of search from the page (header)
-date = get_date(soup)
+init_date = get_date(soup)
 
-table = get_table(soup)
-# print("Table: ", table)
-
-df = pd.DataFrame(table, columns=['Train', 'Stops', 'Departure', 'Duration', 'Price'])
-df = df[df["Train"].apply(lambda x: "AVE" in x)].reset_index(drop=True)
-
-
-def format_time(x):
-    h, m = filter(lambda t: is_number(t), x.split(" "))
-    return datetime.timedelta(hours=int(h), minutes=int(m))
-
-
-df['Duration'] = df['Duration'].apply(lambda x: format_time(x))
-df['Departure'] = df['Departure'].apply(lambda x: datetime.datetime.strptime(str(date)+"-"+x, '%Y-%m-%d-%H.%M'))
-df['Arrival'] = df['Departure'] + df['Duration']
+df = to_dataframe(soup, init_date)
 
 print(df.iloc[-1])
+
+date = init_date
+for i in range(2):
+    # Sum one day to date
+    date += datetime.timedelta(days=1)
+
+    # Get year, month and day from date as strings
+    year, month, day = str(date).split("-")
+
+    # Get day of week starting at sunday = 0
+    weekday = date.weekday() + 1
+
+    url = f'https://horarios.renfe.com/HIRRenfeWeb/buscar.do?O={origin_id}&D={destination_id}&AF={year}&MF={month}&DF={day}&SF={weekday}&ID=s'
+    print("Search url: ", url)
+    print(date)
+
+    req = requests.get(url)
+
+    # Pandas unable to scrap all data, so we use BeautifulSoup to get the rest
+    soup = BeautifulSoup(req.text, 'html.parser')
+
+    df = pd.concat([df, to_dataframe(soup, date)])
+
+print(df.describe())
+print(df.columns)
+print(df['Train'])
+print(df.iloc[-1])
+
+# Save dataframe to csv in datasets folder
+df.to_csv(f"{origin}_{destination}_{init_date}_{date}.csv", index=False)
