10ac5fabd63fc1c83549b6eb8d1d36a8e5f31bdd
DavidMunozValero <david.munoz6@alu.uclm.es>
2022-12-11 12:38:08 +0100

    Renfe GTFS

--- src/robin/offer/renfe_gtfs.py
+++ src/robin/offer/renfe_gtfs.py
@@ -1,4 +1,4 @@
-from src.robin.offer.scraping.renfe import *
+from src.robin.offer.gtfs.renfe import *
 
 savepath = 'renfe_data/'
 url = 'https://ssl.renfe.com/gtransit/Fichero_AV_LD/google_transit.zip'
