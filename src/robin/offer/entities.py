01777739706640d4a96c6947051595b2a507b174
DavidMunozValero <david.munoz6@alu.uclm.es>
2022-12-13 18:44:21 +0100

    PEP

--- src/robin/offer/entities.py
+++ src/robin/offer/entities.py
@@ -1,50 +1,53 @@
 from typing import List, Tuple
+import datetime
 
 
 class Station(object):
-    def __init__(self, id:int, name : str, shortname : str, coords : Tuple[float, float]=None):
-        self.id=id
-        self.name=name
-        self.shortname=shortname
-        self.coords=coords
+    def __init__(self, id_: int, name: str, shortname: str, coords: Tuple[float, float] = None):
+        self.id = id_
+        self.name = name
+        self.shortname = shortname
+        self.coords = coords
     
     def __str__(self):
         return f'[{self.id},{self.name},{self.shortname},{self.coords}]'
     
     
 class TimeSlot(object):
-    def __init__(self, id:int, classmark:float, size:float):
-        self.id=id
-        self.classmark=classmark
-        self.size=size
+    def __init__(self, id_: int, class_mark: float, size: float):
+        self.id = id_
+        self.class_mark = class_mark
+        self.size = size
         
     def __str__(self):
-        return f'[{self.id},{self.classmark},{self.size}]'
+        return f'[{self.id},{self.class_mark},{self.size}]'
 
 
 class Corridor(object):
-    def __init__(self, id:int, liststation:List[Station]=[]):
-        self.id=id
-        self.liststation=liststation
-    def insertStation(self,station:Station):
-        self.liststation.append(station)
+    def __init__(self, id_: int, list_station: List[Station] = []):
+        self.id = id_
+        self.list_station = list_station
+
+    def insert_station(self, station: Station):
+        self.list_station.append(station)
+
     def __str__(self):
-        return f'[{self.id},{self.liststation}]'
+        return f'[{self.id},{self.list_station}]'
 
 
 class Line(object):
-    def __init__(self, id:int, corr:Corridor, serviceType:Tuple=(), timeTable:List[Tuple]=[]):
-        self.id = id
+    def __init__(self, id_: int, corr: Corridor, service_type: Tuple = (), timetable: List[Tuple] = []):
+        self.id = id_
         self.corr = corr
-        #self.lstation = lstation
+        # self.lstation = lstation
 
-        self.J = self.__getstops(serviceType)  # j: Train stops
+        self.J = self.__getstops(service_type)  # j: Train stops
         self.W = self.__getpairs()  # w: Pairs origin-destination
-        self.schedule = timeTable  # TBD - Get schedule - DEFAULT Left to Right
+        self.schedule = timetable  # TBD - Get schedule - DEFAULT Left to Right
 
         # self.timetable=ltimetable # list of tuple (AT,DT)
 
-    def __getstops(self, serviceType):
+    def __getstops(self, servicetype):
         """
         Private method to get the stops of the line
 
@@ -53,7 +56,7 @@ class Line(object):
         Output:
             List of stops
         """
-        return [s for i, s in zip(serviceType, self.corr.liststation) if i]
+        return [s for i, s in zip(servicetype, self.corr.liststation) if i]
 
     def __getpairs(self):
         """
@@ -66,20 +69,20 @@ class Line(object):
         """
         return [(a, b) for i, a in enumerate(self.J) for b in self.J[i + 1:]]
         
-    def insertStation(self,station:Station,AT:float,DT:float):
+    def insert_station(self, station: Station, at: float, dt: float):
         self.lstation.append(station)
-        self.timetable.append((AT,DT)) # TODO: (AT,DT) dependent on travel way --> Not a single (AT, DT) tuple?
+        self.timetable.append((at, dt))  # TODO: (AT,DT) dependent on travel way --> Not a single (AT, DT) tuple?
 
 
 # TODO
 class Seat(object):
     # (id, hard, soft)
-    def __init__(self, id:int, s1:Tuple=(), s2:Tuple=()):
+    def __init__(self, id_: int, s1: Tuple=(), s2: Tuple = ()):
         # s1: hard limit. Linked to capacity constrains (e.g. tickets avaiable)
         # s2: soft limit. (e.g. luggage availability)
-        self.id = id
-        self.s1 = s1
-        self.s3 = s2
+        self.id = id_
+        self.s1 = s1 # Hard limit - Tickets available?
+        self.s3 = s2 # Soft limit - Luggage compartment?
 
     def __str__(self):
         return f'[{self.id}, {self.s1}, {self.s2}]'
@@ -87,8 +90,8 @@ class Seat(object):
 
 # TODO:
 class TSP(object):
-    def __init__(self, id: int, name: str, shortname: str):
-        self.id = id
+    def __init__(self, id_: int, name: str, shortname: str):
+        self.id = id_
         self.name = name
         self.shortname = shortname
 
@@ -98,11 +101,11 @@ class TSP(object):
 
 # TODO:
 class RollingStock(object):
-    def __init__(self, id: int, tsp:TSP, S1:None, Kmax:None):
+    def __init__(self, id: int, tsp: TSP, s1: None, kmax: None):
         self.id = id
         self.tsp = tsp
-        self.S1 = None
-        self.Kmax = None
+        self.S1 = s1
+        self.Kmax = kmax
 
     def __str__(self):
         return f'[{self.id},{self.name},{self.shortname}]'
@@ -111,24 +114,19 @@ class RollingStock(object):
 # TODO:
 class Service(object):
     # (id, date, line, time_slot, seat_type, time_table, price, type_capacity, TSP_capacity, rolling_stock)
-    def __init__(self, id: int, date:None, line:Line):
-        self.id = id
-        self.date = None
-        self.line = None
-        self.timeSlot = None
-        self.seatType = None
-        self.timeTable = None
-        self.price = None
-        self.typeCapacity = None
-        self.tspCapacity = None
-        self.rollingStock = None
+    def __init__(self, id_: int, day: datetime.datetime, line: Line):
+        self.id = id_
+        self.line = line
+        self.timeslot = None
+        self.train = None
+        self.day = None
 
 
 # TODO:
 class Supply(object):
     # (id, date, w(o, d), seat_offer, schedule, price on time t)
-    def __init__(self, id: int, date: None, line: Line):
-        self.id = id
+    def __init__(self, id_: int, date: None, line: Line):
+        self.id = id_
         self.date = None
         self.w = None
         self.seats = None
