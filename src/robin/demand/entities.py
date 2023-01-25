f32bdc04050e1a71de6dfd1c2f025c7a71a9526e
Kinrre <enriqueadrian.villarrubia@alu.uclm.es>
2023-01-25 17:02:30 +0100

    docs: summary class of each entity class

--- src/robin/demand/entities.py
+++ src/robin/demand/entities.py
@@ -4,20 +4,25 @@ class InvalidDistributionException(Exception):
 
 
 class Market:
+    """A market is composed by the origin and destination stations."""
     pass
 
 
 class UserPattern:
+    """A user pattern is a set of random variables and penalties that define it (e.g. business or student)."""
     pass
 
 
 class DemandPattern:
+    """A demand pattern is determined by the potential demand and the distribution of user patterns."""
     pass
 
 
 class Day:
+    """A day is described as its actual date and demand pattern."""
     pass
 
 
 class Passenger:
+    """A passenger is defined by his/her user pattern, the origin-destination pair, the desired day and time of arrival and the day of purchase."""
     pass
