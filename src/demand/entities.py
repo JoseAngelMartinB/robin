cdc2a46c755f7ca082ed2a59abb43a582c2f3b14
Kinrre <enriqueadrian.villarrubia@alu.uclm.es>
2023-01-25 16:32:21 +0100

    chore: boilerplate entities code

---
+++ src/demand/entities.py
@@ -0,0 +1,23 @@
+class InvalidDistributionException(Exception):
+    """Raised when the given distribution is not contained in SciPy."""
+    pass
+
+
+class Market:
+    pass
+
+
+class UserPattern:
+    pass
+
+
+class DemandPattern:
+    pass
+
+
+class Day:
+    pass
+
+
+class Passenger:
+    pass
