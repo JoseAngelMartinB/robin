ff62a7bccc1b7f3c7653f01911746e3c9e47e0eb
Kinrre <enriqueadrian.villarrubia@alu.uclm.es>
2023-01-28 13:14:40 +0100

    feat: invalid distribution exceptions

---
+++ src/robin/demand/exceptions.py
@@ -0,0 +1,13 @@
+class InvalidDistributionException(Exception):
+    """Raised when the given distribution is not contained in SciPy."""
+    pass
+
+
+class InvalidContinuousDistributionException(Exception):
+    """Raised when the given distribution is not a continuous distribution."""
+    pass
+
+
+class InvalidDiscreteDistributionException(Exception):
+    """Raised when the given distribution is not a discrete distribution."""
+    pass
