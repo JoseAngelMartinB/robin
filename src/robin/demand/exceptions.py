d28cc1ded6d31748d50299fa124866e46cad6d80
Kinrre <enriqueadrian.villarrubia@alu.uclm.es>
2023-01-29 12:48:37 +0100

    feat: exceptions messages

--- src/robin/demand/exceptions.py
+++ src/robin/demand/exceptions.py
@@ -2,14 +2,26 @@
 
 class InvalidDistributionException(Exception):
     """Raised when the given distribution is not contained in SciPy."""
-    pass
+    
+    def __init__(self, distribution_name: str, *args, **kwargs):
+        msg = (f"The distribution '{distribution_name}' is not contained in SciPy. "
+               'See details at: https://docs.scipy.org/doc/scipy/reference/stats.html')
+        super().__init__(msg, *args, **kwargs)
 
 
 class InvalidContinuousDistributionException(Exception):
     """Raised when the given distribution is not a continuous distribution."""
-    pass
+
+    def __init__(self, distribution_name: str, *args, **kwargs):
+        msg = (f"The distribution '{distribution_name}' is not a discrete distribution. "
+               'See details at: https://docs.scipy.org/doc/scipy/reference/stats.html#discrete-distributions')
+        super().__init__(msg, *args, **kwargs)
 
 
 class InvalidDiscreteDistributionException(Exception):
     """Raised when the given distribution is not a discrete distribution."""
-    pass
+    
+    def __init__(self, distribution_name: str, *args, **kwargs):
+        msg = (f"The distribution '{distribution_name}' is not a continuous distribution. "
+               'See details at: https://docs.scipy.org/doc/scipy/reference/stats.html#continuous-distributions')
+        super().__init__(msg, *args, **kwargs)
