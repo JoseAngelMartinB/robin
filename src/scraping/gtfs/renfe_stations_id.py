149714499ab2a93867c3eae80d5b774e190200ce
DavidMunozValero <david.munoz6@alu.uclm.es>
2023-01-30 23:12:22 +0100

    Renfe stations .csv generator

--- src/scraping/gtfs/renfe_stations_id.py
+++ src/scraping/gtfs/renfe_stations_id.py
@@ -14,7 +14,6 @@ soup = BeautifulSoup(req.text, 'html.parser')
 stations = get_stations(soup)
 print("Available stations: ", stations)
 
-
 # 2. Retrieve stations from GTFS
 
 # GTFS file
@@ -28,10 +27,11 @@ renfe_gtfs = import_datasets(savepath)
 # Get stations as dataframe from GTFS
 stations_gtfs = renfe_gtfs['stops']
 
-stations_gtfs_dict = {id: name for id, name in zip(stations_gtfs['stop_id'], stations_gtfs['stop_name'])}
+stations_gtfs_dict = {id_: name for id_, name in zip(stations_gtfs['stop_id'], stations_gtfs['stop_name'])}
 
 # Search specific name in GTFS stations
 # Used this to manually find stations that does not match by id (e.g. 'MADRI' vs 60000)
+# DO NOT DELETE
 """
 for id, name in stations_gtfs_dict.items():
     if "Madrid".lower() in name.lower():
@@ -41,47 +41,47 @@ for id, name in stations_gtfs_dict.items():
 # Initialize master dict with stations that match by id between Renfe web and GTFS
 master_dict = {}
 count = 0
-for id, name in stations.items():
+for id_, name in stations.items():
     try:
-        id = int(id)
+        int(id_)
     except ValueError:
         pass
 
-    if id in stations_gtfs_dict.keys():
-        master_dict[id] = (stations_gtfs_dict[id], str(id))
+    if id_ in stations_gtfs_dict.keys():
+        master_dict[id_] = (stations_gtfs_dict[id_], str(id_))
 
 print("Stations that match between Renfe web and GTFS data: ", len(master_dict))
 
-other_dict = {60000: ('Madrid-Puerta de Atocha', 'MADRI'),
-                10500: ('Medina del Campo', 'MEDIN'),
-                31200: ('Puebla de Sanabria', 'PUEBL'),
-                2002: ('Puente Genil-Herrera', 'PTE G'),
-                22300: ('Redondela', 'REDON'),
-                30100: ('Salamanca', 'SALAM'),
-                70101: ('San Fernando Henares', 'S FER'),
-                14223: ('Santander', 'SANTA'),
-                12100: ('Segovia', 'SEGOV'),
-                71500: ('Tarragona', 'TARRA'),
-                65000: ('Valencia-Estacio del Nord', 'VALEN'),
-                8223: ('Vigo Urzaiz', 'VIGO-'),
-                4040: ('Zaragoza-Delicias', 'ZARAG'),
-                2003: ('Antequera-Santa Ana', 'ANTEQ'),
-                71801: ('Barcelona-Sants', 'BARCE'),
-                13200: ('Bilbao-Abando Indalecio Prieto', 'BILBA'),
-                66100: ('Cuenca', 'CUENC'),
-                15410: ('Gijon', 'GIJON'),
-                70200: ('Guadalajara', 'GUADA'),
-                80100: ('Pamplona/irunya', 'IRUN-')}
+other_dict = {'60000': ('Madrid-Puerta de Atocha', 'MADRI'),
+              '10500': ('Medina del Campo', 'MEDIN'),
+              '31200': ('Puebla de Sanabria', 'PUEBL'),
+              '02002': ('Puente Genil-Herrera', 'PTE G'),
+              '22300': ('Redondela', 'REDON'),
+              '30100': ('Salamanca', 'SALAM'),
+              '70101': ('San Fernando Henares', 'S FER'),
+              '14223': ('Santander', 'SANTA'),
+              '12100': ('Segovia', 'SEGOV'),
+              '71500': ('Tarragona', 'TARRA'),
+              '65000': ('Valencia-Estacio del Nord', 'VALEN'),
+              '08223': ('Vigo Urzaiz', 'VIGO-'),
+              '04040': ('Zaragoza-Delicias', 'ZARAG'),
+              '02003': ('Antequera-Santa Ana', 'ANTEQ'),
+              '71801': ('Barcelona-Sants', 'BARCE'),
+              '13200': ('Bilbao-Abando Indalecio Prieto', 'BILBA'),
+              '66100': ('Cuenca', 'CUENC'),
+              '15410': ('Gijon', 'GIJON'),
+              '70200': ('Guadalajara', 'GUADA'),
+              '80100': ('Pamplona/irunya', 'IRUN-')}
 
 # Add missing stations
 master_dict.update(other_dict)
 
-# Add latitud and longitud info to master dict
+# Add latitude and longitude info to master dict
 df = renfe_gtfs['stops']
-for id in master_dict:
-    lat = df[df['stop_id'] == id]['stop_lat'].values[0]
-    lon = df[df['stop_id'] == id]['stop_lon'].values[0]
-    master_dict[id] = list(master_dict[id])+[lat, lon]
+for id_ in master_dict:
+    lat = df[df['stop_id'] == id_]['stop_lat'].values[0]
+    lon = df[df['stop_id'] == id_]['stop_lon'].values[0]
+    master_dict[id_] = list(master_dict[id_]) + [lat, lon]
 
 print(master_dict)
 print("Total stations: ", len(master_dict))
@@ -92,7 +92,11 @@ stop_name, renfe_id, lat, lon = zip(*tuple(master_dict.values()))
 df = pd.DataFrame(list(zip(stop_id, stop_name, renfe_id, lat, lon)),
                   columns=['stop_id', 'stop_name', 'renfe_id', 'stop_lat', 'stop_lon'])
 
+df.loc[-1] = ['00000', 'Unknown', '00000', 0.0, 0.0]
+df.index = df.index + 1  # shifting index
+df.sort_index(inplace=True)
+
 print(df.head())
 
 # Save to csv
-df.to_csv('renfe_stations.csv', index=False)
+df.to_csv('../../../data/scraping/renfe/renfe_stations.csv', index=False)
