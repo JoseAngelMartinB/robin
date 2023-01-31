f2be24fe6bab02e68215ac3b3685a2d048e2b111
DavidMunozValero <david.munoz6@alu.uclm.es>
2023-01-31 13:28:42 +0100

    Fix: Data paths

--- src/robin/supply/supply.py
+++ src/robin/supply/supply.py
@@ -1,18 +1,13 @@
 # Version 0 - Supply Module - 30/01/2023
 
-# Load scraping data and create Supply() object
-
-# Supply() object contains a list of Service() objects that match the user request (origin, destination, date)
-
 from src.robin.supply.entities import *
 from src.scraping.load_data import *
 import pandas as pd
 
-renfe_stations = pd.read_csv('../../../data/scraping/renfe/renfe_stations.csv', delimiter=',', dtype={'stop_id': str})
+renfe_stations = pd.read_csv('data/scraping/renfe/renfe_stations.csv', delimiter=',', dtype={'stop_id': str})
 
 # Load supply data from scraping
-services, seats, corridor, tsp, rolling_stock = load_scraping(f'../../../data/scraping/renfe/trips/trips_MADRI_BARCE_2023-02-01_2023-02-28.csv')
-
+services, seats, corridor, tsp, rolling_stock = load_scraping(f'data/scraping/renfe/trips/trips_MADRI_BARCE_2023-02-01_2023-02-28.csv')
 
 # Request user input: origin, destination, date
 while True:
@@ -33,6 +28,7 @@ dummy_service = services[0]
 print("Service: ", dummy_service)
 print("Line: ", dummy_service.line)
 
+# Supply() obtains a List[Service] that match the user request (origin, destination, datetime.date)
 my_travel = Supply(1, origin, destination, date, services)
 
 print("Supply: ")
