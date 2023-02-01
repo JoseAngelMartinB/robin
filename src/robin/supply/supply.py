0e69c85e47b0bf5dd4eec9cfa66a47a9937da240
DavidMunozValero <david.munoz6@alu.uclm.es>
2023-02-01 13:16:08 +0100

    feat: get_date()

--- src/robin/supply/supply.py
+++ src/robin/supply/supply.py
@@ -1,24 +1,26 @@
-# Version 0 - Supply Module - 30/01/2023
+"""Supply entities from dummy scraping data (Renfe)."""
 
-from src.robin.supply.entities import *
-from src.scraping.load_data import *
+from .entities import Station, TimeSlot, Corridor, Line, Seat, RollingStock, TSP, Service, Supply
+from .utils import get_date
+from src.scraping.load_data import load_scraping
+
 import pandas as pd
 
 renfe_stations = pd.read_csv('data/scraping/renfe/renfe_stations.csv', delimiter=',', dtype={'stop_id': str})
 
 # Load supply data from scraping
-services, seats, corridor, tsp, rolling_stock = load_scraping(f'data/scraping/renfe/trips/trips_MADRI_BARCE_2023-02-01_2023-02-28.csv')
+services, stations, seats, corridor, tsp, rolling_stock = load_scraping(f'data/scraping/renfe/trips/trips_MADRI_BARCE_2023-02-01_2023-02-28.csv')
+
+print("Available stations: ")
+for sid in corridor.stations:
+    print(f'Station ID: {sid} - Station name: {stations[sid].name}')
 
 # Request user input: origin, destination, date
 while True:
-    origin = input("Origin: ")
-    destination = input("Destination: ")
+    origin = input("Departure station ID: ")
+    destination = input("Arrival station ID: ")
 
-    try:
-        date = datetime.datetime.strptime(input("Date (dd-mm-yyyy): "), "%d-%m-%Y").date()
-    except ValueError:
-        print("Invalid date")
-        continue
+    date = get_date(input("Date (dd-mm-yyyy): "))
 
     # Check if origin and destination are in the list of stations operated by Renfe
     if all(s in renfe_stations['stop_id'].values.tolist() for s in (origin, destination)) and origin != destination:
