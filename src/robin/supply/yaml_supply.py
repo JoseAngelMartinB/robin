8d8ee34e614c53f5584ac8cc8bd16c61dfb44c9e
DavidMunozValero <david.munoz6@alu.uclm.es>
2023-02-03 16:59:04 +0100

    Feat: Show available dates

--- src/robin/supply/yaml_supply.py
+++ src/robin/supply/yaml_supply.py
@@ -1,48 +1,25 @@
 """Supply entities from YAML data."""
 
 from src.robin.supply.entities import Supply
-import datetime
+from src.robin.supply.utils import get_date
 
 my_supply = Supply.from_yaml("../../../data/supply_data_example.yml")
-
-print("Stations: ")
-for s in my_supply.stations.values():
-    print(s)
-
-print("Corridors: ")
-for c in my_supply.corridors.values():
-    print(c)
-
-print("Lines: ")
-for ln in my_supply.lines.values():
-    print(ln)
-
-print("RollingStock: ")
-for rs in my_supply.rollingStock.values():
-    print(rs)
-
-print("Seats: ")
-for st in my_supply.seats.values():
-    print(st)
-
-print("TSPs: ")
-for tsp in my_supply.tsps.values():
-    print(tsp)
-
-print("Services: ")
-for s in my_supply.services.values():
-    print(s)
 
 print("Available stations: ")
 for sid in my_supply.stations:
     print(f'Station ID: {sid} - Station name: {my_supply.stations[sid].name}')
 print()
 
-origin = "BCN"
-destination = "MAD"
-date = datetime.datetime(day=22, month=1, year=2023).date()
+print("Available dates: ")
+dates = [sv.date for sv in my_supply.services.values()]
+print(f'From: {min(dates)} to {max(dates)}\n')
 
-services = my_supply.generate('BCN', 'MAD', datetime.datetime(year=2023, month=1, day=22).date())
+origin = input("Origin station ID: ")
+destination = input("Destination station ID: ")
+
+date = get_date(input("Date (YYYY-MM-DD): "))
+
+services = my_supply.generate(origin, destination, date)
 
 print(f'Travel options - Travel from {origin} to {destination} on {date}:')
 for s in services:
