41320cdabde153d6631fa1c687f36186d3c163e7
DavidMunozValero <david.munoz6@alu.uclm.es>
2023-02-03 14:14:26 +0100

    Test entities from YML

--- src/robin/supply/yaml_supply.py
+++ src/robin/supply/yaml_supply.py
@@ -5,6 +5,34 @@ import datetime
 
 my_supply = Supply.from_yaml("../../../data/supply_data_example.yml")
 
+print("Stations: ")
+for s in my_supply.stations.values():
+    print(s)
+
+print("Corridors: ")
+for c in my_supply.corridors.values():
+    print(c)
+
+print("Lines: ")
+for ln in my_supply.lines.values():
+    print(ln)
+
+print("RollingStock: ")
+for rs in my_supply.rollingStock.values():
+    print(rs)
+
+print("Seats: ")
+for st in my_supply.seats.values():
+    print(st)
+
+print("TSPs: ")
+for tsp in my_supply.tsps.values():
+    print(tsp)
+
+print("Services: ")
+for s in my_supply.services.values():
+    print(s)
+
 print("Available stations: ")
 for sid in my_supply.stations:
     print(f'Station ID: {sid} - Station name: {my_supply.stations[sid].name}')
