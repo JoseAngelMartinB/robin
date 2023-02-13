3996f9686fe483d8ddc50d110bb56b8863fd4310
DavidMunozValero <david.munoz6@alu.uclm.es>
2023-02-14 00:07:27 +0100

    fix: exception messages

--- src/robin/supply/exceptions.py
+++ src/robin/supply/exceptions.py
@@ -2,17 +2,17 @@
 
 
 class InvalidTimeStringFormat(Exception):
-    """Raise when string format doesn't match HH:MM format."""
+    """Raise when string format doesn't match HH:MM:SS format."""
     def __init__(self, time: str):
         msg = (f"Invalid time format: {time}. "
-               'Time must be a string in format HH:SS')
+               'Time must be a string in format HH:MM:SS')
         # super() inherits from Exception class
         super().__init__(msg)
 
 
 class InvalidDateStringFormat(Exception):
-    """Raise when string format doesn't match dd-mm-YYYY format."""
+    """Raise when string format doesn't match YYYY-mm-dd format."""
     def __init__(self, date: str):
         msg = (f"Invalid date format: {date}. "
-               'Date must be a string in format dd-mm-YYYY')
+               'Date must be a string in format YYYY-mm-dd')
         super().__init__(msg)
