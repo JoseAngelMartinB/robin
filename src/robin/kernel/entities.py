a3523743e0419562b8e5c56f586ea4f152bde1bd
Kinrre <enriqueadrian.villarrubia@alu.uclm.es>
2023-02-14 13:48:29 +0100

    feat: simulate kernel method

--- src/robin/kernel/entities.py
+++ src/robin/kernel/entities.py
@@ -1,4 +1,6 @@
-from src.robin.demand.entities import Day, Demand
+"""Entities for the kernel module."""
+
+from src.robin.demand.entities import Demand
 from src.robin.supply.entities import Service, Supply
 from typing import List
 
@@ -15,14 +17,46 @@ class Kernel:
         passengers = self.demand.generate_passengers()
         
         for passenger in passengers:
-            # Filter services by passenger's origin and destination and date
+            # Filter services by passenger's origin-destination and date
+            origin = passenger.market.departure_station
+            destination = passenger.market.arrival_station
             services = self.supply.filter_services(
                 origin=passenger.market.departure_station,
                 destination=passenger.market.arrival_station,
                 date=passenger.arrival_day.date
             )
 
-            # Calculate utility for each service
+            # Calculate utility for each service and seat
+            service_arg_max = None
+            seat_arg_max = None
+            seat_utility = 0
+
+            for service in services:
+                for seat in service.prices[(origin, destination)].keys():
+                    utility = passenger.get_utility(
+                        seat=seat.id,
+                        service_departure_time=service.service_departure_time,
+                        service_arrival_time=service.service_arrival_time,
+                        price=service.prices[(origin, destination)][seat],
+                        departure_time_hard_restriction=False
+                    )
+                    # Update service with max utility
+                    if utility > seat_utility:
+                        service_arg_max = service
+                        seat_arg_max = seat
+                        seat_utility = utility
+
+            # Buy ticket if utility is positive
+            if seat_utility > 0:
+                assert service_arg_max is not None
+                assert seat_arg_max is not None
+                service_arg_max.buy_ticket(
+                    origin=passenger.market.departure_station,
+                    destination=passenger.market.arrival_station,
+                    seat=seat_arg_max
+                )
+
+        return self.supply.services
     
     def set_seed(self, seed: int):
         pass
