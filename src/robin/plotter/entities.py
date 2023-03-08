80aa8f89aea938d3cacd3888d0ab8e8948679cf7
DavidMunozValero <david.munoz6@alu.uclm.es>
2023-03-08 13:58:30 +0100

    fix: formatted plot

--- src/robin/plotter/entities.py
+++ src/robin/plotter/entities.py
@@ -9,26 +9,40 @@ def plot_data(data: Dict[datetime.date, Dict[str, Dict[str, int]]]):
     user_types = sorted(set(ut for d in data for ut in data[d]))
     seat_types = sorted(set(st for d in data for ut in data[d] for st in data[d][ut]))
 
-    fig, axs = plt.subplots(len(user_types), 1, figsize=(8, 3 * len(user_types)))
-    fig.subplots_adjust(hspace=0.5)
+    plt.style.use('seaborn-pastel')
 
-    colors = ['red', 'green', 'blue', 'purple', 'orange']
+    fig, axs = plt.subplots(len(user_types), 1, figsize=(7, 4 * len(user_types)))
+    fig.subplots_adjust(hspace=0.75, bottom=0.2, top=0.9)
+
+    colors = ['lemonchiffon', 'lightsteelblue', 'palegreen', 'lightsalmon', 'lavender', 'lightgray']
+    dark_colors = ['gold', 'royalblue', 'darkgreen', 'darkred', 'mediumpurple', 'dimgray']
+    colors_dict = {c: dc for c, dc in zip(colors, dark_colors)}
 
     for i, user_type in enumerate(user_types):
         ax = axs[i]
-        ax.set_title(f'Total de tickets vendidos para el tipo de usuario "{user_type}"')
+        ax.set_facecolor('#F5F5F5')
+        ax.set_title(f'Total de tickets vendidos para el tipo de usuario "{user_type}"', fontweight='bold')
         ax.set_ylabel('Número de tickets')
-        ax.set_xticks(np.arange(len(data)) + 0.2)
-        ax.set_xticklabels(data.keys())
-        ax.set_xlim([-0.2, len(data)])
+        ax.set_xlabel('Fecha de llegada', labelpad=10)
+        ax.set_xticks(np.arange(len(data)))
+        ax.set_xticklabels(data.keys(), rotation=60, fontsize=8)
+        ax.set_xlim([-0.5, len(data)])
 
         bottom = np.zeros(len(data))
         for j, seat_type in enumerate(seat_types):
             color = colors[j % len(colors)]
             values = [data[date].get(user_type, {}).get(seat_type, 0) for date in data.keys()]
-            ax.bar(np.arange(len(data)), values, bottom=bottom, color=color, label=seat_type)
+            ax.bar(np.arange(len(data)), values,
+                   width=0.5,
+                   bottom=bottom,
+                   color=color,
+                   label=seat_type,
+                   edgecolor=colors_dict[color],
+                   linewidth=0.5,
+                   zorder=2)
             bottom += values
 
+        ax.grid(axis='y', color='#A9A9A9', alpha=0.3, zorder=1)
         ax.legend()
     plt.show()
 
@@ -46,7 +60,7 @@ if __name__ == "__main__":
 
     data = {}
     for row in df.iterrows():
-        day, user, seat = tuple(row[1][["purchase_day", "user_pattern", "seat"]])
+        day, user, seat = tuple(row[1][["arrival_day", "user_pattern", "seat"]])
 
         if day not in data:
             data[day] = {}
@@ -61,6 +75,7 @@ if __name__ == "__main__":
 
         data[day][user][seat] += 1
 
+    sorted_data = dict(sorted(data.items(), key=lambda x: x[0]))
     print("Data is ready. Plotting...")
 
-    plot_data(data)
+    plot_data(sorted_data)
