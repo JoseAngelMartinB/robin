4ea37ee55f77490f5c198d504a746ae49022b848
Kinrre <enriqueadrian.villarrubia@alu.uclm.es>
2023-03-11 13:41:23 +0100

    fix: rename linear function parameters to betas

--- src/robin/demand/functions.py
+++ src/robin/demand/functions.py
@@ -9,16 +9,16 @@ class Function:
     """
 
     @staticmethod
-    def linear(m: float, x: float, b: float) -> float:
+    def linear(x: float, b_0: float, b_1: float) -> float:
         """
         Linear function.
 
         Args:
-            m (float): The slope.
             x (float): The x value.
-            b (float): The y-intercept.
+            b_0 (float): The y-intercept.
+            b_1 (float): The slope.
 
         Returns:
             float: The y value.
         """
-        return m * x + b
+        return b_0 + b_1 * x
