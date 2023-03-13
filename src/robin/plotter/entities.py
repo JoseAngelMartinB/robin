b117698cc3900afd550c452aeb015c6208e5ddd9
DavidMunozValero <david.munoz6@alu.uclm.es>
2023-03-13 12:59:01 +0100

    fix: plot total tickets sold in a day

--- src/robin/plotter/entities.py
+++ src/robin/plotter/entities.py
@@ -5,23 +5,53 @@ import numpy as np
 import datetime
 
 
-def plot_data(data: Dict[datetime.date, Dict[str, Dict[str, int]]]):
-    user_types = sorted(set(ut for d in data for ut in data[d]))
-    seat_types = sorted(set(st for d in data for ut in data[d] for st in data[d][ut]))
+class KernelPlotter:
+    def __init__(self, path: str):
+        self.df = pd.read_csv(path)
 
-    plt.style.use('seaborn-pastel')
+        self.df["purchase_day"] = self.df.apply(
+            lambda row: self._get_purchase_day(row["purchase_day"], row["arrival_day"]), axis=1
+        )
 
-    fig, axs = plt.subplots(len(user_types), 1, figsize=(7, 4 * len(user_types)))
-    fig.subplots_adjust(hspace=0.75, bottom=0.2, top=0.9)
+        plt.style.use('seaborn-pastel')
+        colors = ['lemonchiffon', 'lightsteelblue', 'palegreen', 'lightsalmon', 'lavender', 'lightgray']
+        dark_colors = ['gold', 'royalblue', 'darkgreen', 'darkred', 'mediumpurple', 'dimgray']
+        self.colors_dict = {c: dc for c, dc in zip(colors, dark_colors)}
 
-    colors = ['lemonchiffon', 'lightsteelblue', 'palegreen', 'lightsalmon', 'lavender', 'lightgray']
-    dark_colors = ['gold', 'royalblue', 'darkgreen', 'darkred', 'mediumpurple', 'dimgray']
-    colors_dict = {c: dc for c, dc in zip(colors, dark_colors)}
+    def _get_purchase_day(self, anticipation, arrival_day):
+        anticipation = datetime.timedelta(days=anticipation)
+        arrival_day = datetime.datetime.strptime(arrival_day, "%Y-%m-%d")
+        purchase_day = arrival_day - anticipation
+        return purchase_day.date()
 
-    for i, user_type in enumerate(user_types):
-        ax = axs[i]
+    def _get_total_tickets_sold(self):
+        data = {}
+        for row in self.df.iterrows():
+            day, seat = tuple(row[1][["purchase_day", "seat"]])
+
+            if day not in data:
+                data[day] = {}
+
+            if seat is np.nan:
+                continue
+
+            if seat not in data[day]:
+                data[day][seat] = 0
+
+            data[day][seat] += 1
+
+        sorted_data = dict(sorted(data.items(), key=lambda x: x[0]))
+        return sorted_data
+
+    def plot_tickets_sold(self, save_path: str = None):
+        data = self._get_total_tickets_sold()
+        seat_types = sorted(set(st for d in data for st in data[d]))
+
+        fig, ax = plt.subplots(1, 1, figsize=(7, 4))
+        fig.subplots_adjust(hspace=0.75, bottom=0.2, top=0.9)
+
         ax.set_facecolor('#F5F5F5')
-        ax.set_title(f'Total de tickets vendidos para el tipo de usuario "{user_type}"', fontweight='bold')
+        ax.set_title(f'Total de tickets vendidos por día', fontweight='bold')
         ax.set_ylabel('Número de tickets')
         ax.set_xlabel('Fecha de llegada', labelpad=10)
         ax.set_xticks(np.arange(len(data)))
@@ -30,54 +60,87 @@ def plot_data(data: Dict[datetime.date, Dict[str, Dict[str, int]]]):
 
         bottom = np.zeros(len(data))
         for j, seat_type in enumerate(seat_types):
-            color = colors[j % len(colors)]
-            values = [data[date].get(user_type, {}).get(seat_type, 0) for date in data.keys()]
+            color = list(self.colors_dict.keys())[j % len(self.colors_dict.keys())]
+            values = [data[date].get(seat_type, 0) for date in data.keys()]
             ax.bar(np.arange(len(data)), values,
                    width=0.5,
                    bottom=bottom,
                    color=color,
                    label=seat_type,
-                   edgecolor=colors_dict[color],
+                   edgecolor=self.colors_dict[color],
                    linewidth=0.5,
                    zorder=2)
             bottom += values
 
         ax.grid(axis='y', color='#A9A9A9', alpha=0.3, zorder=1)
         ax.legend()
-    plt.show()
+        plt.show()
 
-    fig.savefig('output_new.png', dpi=300, format='png')
+        if save_path is not None:
+            fig.savefig(save_path, dpi=300, bbox_inches='tight')
+
+    def _get_tickets_sold_by_user(self):
+        data = {}
+        for row in self.df.iterrows():
+            day, user, seat = tuple(row[1][["purchase_day", "user_pattern", "seat"]])
+
+            if day not in data:
+                data[day] = {}
+            if user not in data[day]:
+                data[day][user] = {}
+
+            if seat is np.nan:
+                continue
+
+            if seat not in data[day][user]:
+                data[day][user][seat] = 0
+
+            data[day][user][seat] += 1
+
+        sorted_data = dict(sorted(data.items(), key=lambda x: x[0]))
+        return sorted_data
+
+    def plot_tickets_by_user(self, save_path: str = None):
+        data = self._get_tickets_sold_by_user()
+        user_types = sorted(set(ut for d in data for ut in data[d]))
+        seat_types = sorted(set(st for d in data for ut in data[d] for st in data[d][ut]))
+
+        fig, axs = plt.subplots(len(user_types), 1, figsize=(7, 4 * len(user_types)))
+        fig.subplots_adjust(hspace=0.75, bottom=0.2, top=0.9)
+
+        for i, user_type in enumerate(user_types):
+            ax = axs[i]
+            ax.set_facecolor('#F5F5F5')
+            ax.set_title(f'Total de tickets vendidos para el tipo de usuario "{user_type}"', fontweight='bold')
+            ax.set_ylabel('Número de tickets')
+            ax.set_xlabel('Fecha de llegada', labelpad=10)
+            ax.set_xticks(np.arange(len(data)))
+            ax.set_xticklabels(data.keys(), rotation=60, fontsize=8)
+            ax.set_xlim([-0.5, len(data)])
+
+            bottom = np.zeros(len(data))
+            for j, seat_type in enumerate(seat_types):
+                color = list(self.colors_dict.keys())[j % len(self.colors_dict.keys())]
+                values = [data[date].get(user_type, {}).get(seat_type, 0) for date in data.keys()]
+                ax.bar(np.arange(len(data)), values,
+                       width=0.5,
+                       bottom=bottom,
+                       color=color,
+                       label=seat_type,
+                       edgecolor=self.colors_dict[color],
+                       linewidth=0.5,
+                       zorder=2)
+                bottom += values
+
+            ax.grid(axis='y', color='#A9A9A9', alpha=0.3, zorder=1)
+            ax.legend()
+        plt.show()
+
+        if save_path is not None:
+            fig.savefig(save_path, dpi=300, bbox_inches='tight')
 
 
 if __name__ == "__main__":
-    df = pd.read_csv("../kernel/output_renfe_new.csv")
+    kernel_plotter = KernelPlotter(path="../kernel/output_renfe_new.csv")
 
-    def get_purchase_day(anticipation, arrival_day):
-        anticipation = datetime.timedelta(days=anticipation)
-        arrival_day = datetime.datetime.strptime(arrival_day, "%Y-%m-%d")
-        purchase_day = arrival_day - anticipation
-        return purchase_day.date()
-
-    df["purchase_day"] = df.apply(lambda row: get_purchase_day(row["purchase_day"], row["arrival_day"]), axis=1)
-
-    data = {}
-    for row in df.iterrows():
-        day, user, seat = tuple(row[1][["purchase_day", "user_pattern", "seat"]])
-
-        if day not in data:
-            data[day] = {}
-        if user not in data[day]:
-            data[day][user] = {}
-
-        if seat is np.nan:
-            continue
-
-        if seat not in data[day][user]:
-            data[day][user][seat] = 0
-
-        data[day][user][seat] += 1
-
-    sorted_data = dict(sorted(data.items(), key=lambda x: x[0]))
-    print("Data is ready. Plotting...")
-
-    plot_data(sorted_data)
+    kernel_plotter.plot_tickets_sold(save_path="total_tickets_sold.png")
