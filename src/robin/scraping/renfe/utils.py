1386f01a08331daf3d7714282ff9672a694303b4
Kinrre <enriqueadrian.villarrubia@alu.uclm.es>
2023-04-11 20:47:55 +0200

    docs: renfe scraping utils docstring formatting

--- src/robin/scraping/renfe/utils.py
+++ src/robin/scraping/renfe/utils.py
@@ -2,52 +2,18 @@
 
 import re
 
-def is_number(s: str) -> bool:
-    """
-    Function to check if a string is a number
-
-    Args:
-        s (str): String to check
-
-    Returns:
-        bool: True if string is a number, False otherwise
-    """
-    try:
-        float(s)
-        return True
-    except ValueError:
-        return False
-
-
-def time_to_minutes(time: str) -> int:
-    """
-    Function receives "time", a string with time formatted as "HH.MM" and returns the number of minutes
-
-    Args:
-        time (str): String with time formatted as "HH.MM"
-
-    Returns:
-        int: Number of minutes
-    """
-    try:
-        h, m = time.split(".")
-    except ValueError:
-        return 0
-
-    return int(h) * 60 + int(m)
-
 
 def format_duration(x: str) -> int:
     """
-    Function receives "x", a string with time formatted as "2 h. 30 m." and returns a string H:M
+    Convert string duration to minutes.
 
     Args:
-        x (str): String with time formatted as "2 h. 30 m."
+        x (str): String duration.
 
     Returns:
-        str: String with duration in minutes
+        int: Duration in minutes.
     """
-    tuple_hour_min = tuple(filter(lambda t: is_number(t), x.split(" ")))
+    tuple_hour_min = tuple(filter(lambda t: is_number(t), x.split(' ')))
     if len(tuple_hour_min) == 1:
         m = tuple_hour_min[0]
         return int(m)
@@ -55,15 +21,49 @@ def format_duration(x: str) -> int:
     return int(h) * 60 + int(m)
 
 
-def remove_blanks(s: str, replace_by: str = '') -> str:
+def is_number(x: str) -> bool:
+    """
+    Function to check if a string is a number.
+
+    Args:
+        x (str): String to check.
+
+    Returns:
+        bool: True if x is a number, False otherwise.
+    """
+    try:
+        float(x)
+        return True
+    except ValueError:
+        return False
+
+
+def remove_blanks(x: str, replace_by: str = '') -> str:
     """
     Removes blank spaces (tabs, newlines, etc.) from a string.
 
     Args:
-        s (str): string to remove blank spaces from
-        replace_by (str): string to replace the blank spaces with
+        x (str): String to remove blank spaces from.
+        replace_by (str): String to replace the blank spaces with.
 
     Returns:
-        str: string without blank spaces
+        str: String without blank spaces.
     """
-    return re.sub(r'\s+', replace_by, s)
+    return re.sub(r'\s+', replace_by, x)
+
+
+def time_to_minutes(time: str) -> int:
+    """
+    Convert time string formated as 'HH.MM' to minutes.
+
+    Args:
+        time (str): Time string formated as 'HH.MM'.
+
+    Returns:
+        int: Time in minutes.
+    """
+    try:
+        h, m = time.split('.')
+    except ValueError:
+        return 0
+    return int(h) * 60 + int(m)
