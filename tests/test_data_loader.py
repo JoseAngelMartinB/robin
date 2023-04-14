7cecb3ba3b0f565d5ddf4b5666dd89d95a765f55
DavidMunozValero <david.munoz6@alu.uclm.es>
2023-04-14 13:40:49 +0200

    fix: updated import path

--- tests/test_data_loader.py
+++ tests/test_data_loader.py
@@ -1,6 +1,7 @@
-from src.robin.scraping.entities import DataLoader, IMPORT_PATH
+from src.robin.scraping.entities import DataLoader
 
-trips_path = f'{IMPORT_PATH}/trips/trips_MADRI_BARCE_2023-06-01_2023-06-02.csv'
+import_path = 'data/renfe'
+trips_path = f'{import_path}/trips/trips_MADRI_BARCE_2023-06-01_2023-06-02.csv'
 
 data_loader = DataLoader(trips_path)
 data_loader.show_metadata()
