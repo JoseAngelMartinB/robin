944261847f183863fdb2ea38d6e85014b6f70c3c
DavidMunozValero <david.munoz6@alu.uclm.es>
2023-06-24 01:25:12 +0200

    fix: updated test file to avoid redundant method to save yaml file

--- tests/test_data_loader.py
+++ tests/test_data_loader.py
@@ -1,12 +1,13 @@
-from src.robin.scraping.entities import DataLoader
+from src.robin.scraping.entities import DataLoader, SupplySaver
 
 import_path = 'data/renfe'
-trips_path = f'{import_path}/stop_times/stopTimes_MADRI_BARCE_2023-06-01_2023-06-02.csv'
+trips_path = f'{import_path}/stop_times/stopTimes_MADRI_BARCE_2023-06-23_2023-06-24.csv'
 
 data_loader = DataLoader(trips_path)
 data_loader.show_metadata()
 
 data_loader.build_supply_entities()
-print(list(data_loader.services.values())[1])
+print(data_loader.services[0])
 
-data_loader.save_yaml(save_path='configs/test_case/supply_data.yml')
+services = data_loader.services
+SupplySaver(services).to_yaml(filename='supply_data_23Jun.yml', save_path='data/')
