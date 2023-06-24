37474b035566381ede39f6c89c24fd0ea619b922
DavidMunozValero <david.munoz6@alu.uclm.es>
2023-06-24 02:00:04 +0200

    feat: test simulation loop

--- tests/test_labs.py
+++ tests/test_labs.py
@@ -1,6 +1,6 @@
 from src.robin.labs.entities import RobinLab
 
-path_config_supply = 'configs/supply_data.yml'
+path_config_supply = 'configs/supply_data_23Jun.yml'
 path_config_demand = 'configs/demand_data.yml'
 
 config = {"supply": {"start": 0.0, "stop": 10.0, "step": 1.0},
@@ -9,3 +9,5 @@ config = {"supply": {"start": 0.0, "stop": 10.0, "step": 1.0},
 robin_lab = RobinLab(path_config_supply="configs/test_case/supply_data.yml",
                      path_config_demand="configs/test_case/demand_data.yml",
                      lab_config=config)
+
+robin_lab.simulate()
