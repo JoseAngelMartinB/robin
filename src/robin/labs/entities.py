e6f4beb23483f82ce8f2e23a91f6bc25ed1a529d
DavidMunozValero <david.munoz6@alu.uclm.es>
2023-06-26 00:39:18 +0200

    fix: sort file processing

--- src/robin/labs/entities.py
+++ src/robin/labs/entities.py
@@ -12,7 +12,9 @@ from src.robin.scraping.entities import SupplySaver
 from copy import deepcopy
 from typing import Mapping
 
+from tqdm.auto import tqdm
 from matplotlib import pyplot as plt
+
 
 class RobinLab:
     """Robin Lab experiment."""
@@ -21,7 +23,8 @@ class RobinLab:
                  path_config_supply: str,
                  path_config_demand: str,
                  tmp_path: str,
-                 lab_config: Mapping):
+                 lab_config: Mapping,
+                 verbose=0):
         """
         Initialize Robin Lab experiment.
 
@@ -36,14 +39,14 @@ class RobinLab:
         if not os.path.exists(self.tmp_path):
             os.makedirs(self.tmp_path)
 
-        if not os.path.exists(self.tmp_path+"/supply"):
-            os.makedirs(self.tmp_path+"/supply")
+        if not os.path.exists(self.tmp_path + "/supply"):
+            os.makedirs(self.tmp_path + "/supply")
 
-        if not os.path.exists(self.tmp_path+"/demand"):
-            os.makedirs(self.tmp_path+"/demand")
+        if not os.path.exists(self.tmp_path + "/demand"):
+            os.makedirs(self.tmp_path + "/demand")
 
-        if not os.path.exists(self.tmp_path+"/output"):
-            os.makedirs(self.tmp_path+"/output")
+        if not os.path.exists(self.tmp_path + "/output"):
+            os.makedirs(self.tmp_path + "/output")
 
         self.lab_config = lab_config
         self.default_supply = Supply.from_yaml(path_config_supply)
@@ -62,18 +65,21 @@ class RobinLab:
         self.sim_sequence = {}
 
     def _supply_yaml_editor(self):
+        def multiply_price_values(dictionary: Mapping, factor: float):
+            for key, value in dictionary.items():
+                if isinstance(value, dict):
+                    multiply_price_values(value, factor)
+                elif isinstance(value, (int, float)):
+                    dictionary[key] *= (1 + factor / 100)
+
         supply_lab_config = self.lab_config["supply"]
 
         print(f"Generating supply data...")
         arange_args = supply_lab_config
-        for i in np.arange(**arange_args):
-            tmp_services = []
-            for service in self.default_supply.services:
-                tmp_service = deepcopy(service)
-                for origin_destination in tmp_service.prices:
-                    for seat in tmp_service.prices[origin_destination]:
-                        tmp_service.prices[origin_destination][seat] *= (1 + i/100)
-                tmp_services.append(tmp_service)
+        for i, factor in enumerate(tqdm(np.arange(**arange_args)), start=1):
+            tmp_services = deepcopy(self.default_supply.services)
+            for tmp_service in tmp_services:
+                multiply_price_values(tmp_service.prices, factor)
 
             supply_file_name = f"/supply_{i}.yml"
             SupplySaver(services=tmp_services).to_yaml(filename=supply_file_name, save_path=self.tmp_path+"/supply")
@@ -81,26 +87,27 @@ class RobinLab:
     def simulate(self):
         """Simulate the experiment."""
         # For each supply file, run the simulation.
-        for supply_file in sorted(os.listdir(self.tmp_path+"/supply")):
-            kernel = Kernel(path_config_supply=self.tmp_path+"/supply/"+supply_file,
-                            path_config_demand=self.tmp_path+"/demand/"+os.path.basename(self.path_config_demand))
-            kernel.simulate(output_path=self.tmp_path+"/output/output_"+supply_file.split("_")[-1][:-3]+".csv")
-            print("Successfully simulated supply file: ", supply_file)
+        for i, supply_file in enumerate(tqdm(sorted(os.listdir(self.tmp_path + "/supply"), key=lambda x: int(x.split(".")[0].split("_")[-1]))), start=1):
+            kernel = Kernel(path_config_supply=self.tmp_path + "/supply/" + supply_file,
+                            path_config_demand=self.tmp_path + "/demand/" + os.path.basename(self.path_config_demand))
+            kernel.simulate(output_path=f"{self.tmp_path}/output/output_{i}.csv")
 
     def _get_tickets_sold(self):
         tickets_sold = {}
-        for output_file in sorted(os.listdir(self.tmp_path + "/output/")):
+        for output_file in sorted(os.listdir(self.tmp_path + "/output"), key=lambda x: int(x.split(".")[0].split("_")[-1])):
             df = pd.read_csv(self.tmp_path + "/output/" + output_file)
             buffer_tickets_sold = df.groupby(by=['seat']).size().to_dict()
             buffer_tickets_sold['Total'] = sum(buffer_tickets_sold.values())
-            tickets_sold[output_file.split("_")[-1][:-5]] = buffer_tickets_sold
+            tickets_sold[output_file.split(".")[0].split("_")[-1]] = buffer_tickets_sold
 
         return tickets_sold
 
     def plot_elasticity_curve(self):
         tickets_sold = self._get_tickets_sold()
 
-        t = tickets_sold.keys()
+        supply_lab_config = self.lab_config["supply"]
+        arange_args = supply_lab_config
+        t = np.arange(**arange_args)
         series_keys = set(k for v in tickets_sold.values() for k in v)
         series = {k: [] for k in series_keys}
 
@@ -109,8 +116,12 @@ class RobinLab:
                 series[kk].append(tickets_sold[k].get(kk, 0))
 
         plt.figure(figsize=(10, 5))
+        plt.title("Price elasticity curve")
         for k in series:
             plt.plot(t, series[k], label=k)
 
+        plt.xticks(ticks=t[::3], labels=[f"{x:.0f}%" for x in t][::3])
+        plt.xlabel("Price increase (%)")
+        plt.ylabel("Tickets sold")
         plt.legend()
         plt.show()
