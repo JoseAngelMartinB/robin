5acbfbae41765d502b6c062a501fcf739620008d
Kinrre <EnriqueAdrian.Villarrubia@alu.uclm.es>
2025-05-12 10:41:39 +0200

    fix: remove ServiceInMultiplePathsException in supply generator

--- src/robin/supply/generator/exceptions.py
+++ src/robin/supply/generator/exceptions.py
@@ -1,19 +1,11 @@
 """Exceptions for the supply generator module."""
 
 
-class ServiceInMultiplePathsException(Exception):
-    """Raised when a service is in multiple paths."""
-    
-    def __init__(self, *args, **kwargs):
-        msg = 'The service is in multiple paths.'
-        super().__init__(msg, *args, **kwargs)
-
-
 class ServiceWithConflicts(Exception):
-    """Raised when a service is in multiple paths."""
+    """Raised when a service has conflicts with existing services."""
 
     def __init__(self, *args, **kwargs):
-        msg = 'The service has conflicts'
+        msg = 'The service has conflicts with existing services.'
         super().__init__(msg, *args, **kwargs)
 
 
