c1969b4a6eccfc21be012be523e313d389fac5f1
Kinrre <enriqueadrian.villarrubia@alu.uclm.es>
2025-05-13 18:27:42 +0200

    fix: add chrome arguments to run in docker

--- src/robin/scraping/renfe/entities.py
+++ src/robin/scraping/renfe/entities.py
@@ -47,6 +47,8 @@ class DriverManager:
         driver_options = Options()
         driver_options.add_argument('--disable-extensions')
         driver_options.add_argument('--headless')
+        driver_options.add_argument('--no-sandbox')
+        driver_options.add_argument('--disable-dev-shm-usage')
         self.driver = webdriver.Chrome(options=driver_options)
         self.stations_df = stations_df
         self.allowed_train_types = allowed_train_types
